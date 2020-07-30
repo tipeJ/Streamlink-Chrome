80489bfc020a03dbcb06acf457814209dd848130
tipej <tiitus.jarvinen@gmail.com>
2020-07-31 00:38:56 +0300

    Added proper youtube player button injection

--- app/youtube_inject.js
+++ app/youtube_inject.js
@@ -3,9 +3,14 @@ launchButton.addEventListener("click", function() {
     chrome.runtime.sendMessage({Message: "launchStream"}, function (response) {
     });
 });
-var imgURL = chrome.extension.getURL("assets/icon-22.png");
-launchButton.innerHTML = "<img src=" + imgURL + ">";
-launchButton.className = "streamlinkchromebutton";
+
+var imgURL = chrome.extension.getURL("assets/icon.svg");
+var iitem = document.getElementsByClassName("ytp-settings-button")[0];
+
+launchButton.innerHTML = '<img src="' + imgURL + '" width="75%" height=75%">';
+launchButton.className = "ytp-button ytp-settings-button";
+launchButton.setAttribute("aria-label", "Streamlink");
+launchButton.setAttribute("title", "Streamlink");
 var controlsRow = document.getElementsByClassName('ytp-right-controls')
 controlsRow[0]
     .insertBefore(launchButton, controlsRow[0].childNodes[4]);

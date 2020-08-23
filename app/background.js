17144cd0e6d5621639670c0f24fb7e9ddc867e8c
tipej <tiitus.jarvinen@gmail.com>
2020-08-24 00:05:06 +0300

    Removed page action rules

--- app/background.js
+++ app/background.js
@@ -34,26 +34,26 @@ chrome.runtime.onInstalled.addListener(function() {
         fetch(allowedDomainsUrl).then((response) => {
             response.text().then(function (text) {
                 // Replace all rules ...
-                chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-                    // With a new rule ...
-                    var domainsList = text.replace(/\./g, "\\.").split('\n');
-                    var urlConditions = "";
-                    for (i = 0; i < domainsList.length; i++) {
-                        urlConditions = urlConditions.concat('^'.concat('(' + domainsList[i].trim() + ')'));
-                        if (i != domainsList.length - 1) {
-                            urlConditions = urlConditions.concat('|');
-                        }
-                    }
-                    var newRule = {
-                        // That fires when a page's URL contains one of the supported commands.
-                        conditions: [new chrome.declarativeContent.PageStateMatcher({
-                            pageUrl: { urlMatches: urlConditions },
-                        })],
-                        // And shows the extension's page action.
-                        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-                    };
-                    chrome.declarativeContent.onPageChanged.addRules([newRule]);
-                });
+                // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+                //     // With a new rule ...
+                //     var domainsList = text.replace(/\./g, "\\.").split('\n');
+                //     var urlConditions = "";
+                //     for (i = 0; i < domainsList.length; i++) {
+                //         urlConditions = urlConditions.concat('^'.concat('(' + domainsList[i].trim() + ')'));
+                //         if (i != domainsList.length - 1) {
+                //             urlConditions = urlConditions.concat('|');
+                //         }
+                //     }
+                //     var newRule = {
+                //         // That fires when a page's URL contains one of the supported commands.
+                //         conditions: [new chrome.declarativeContent.PageStateMatcher({
+                //             pageUrl: { urlMatches: urlConditions },
+                //         })],
+                //         // And shows the extension's page action.
+                //         actions: [ new chrome.declarativeContent.ShowPageAction() ]
+                //     };
+                //     chrome.declarativeContent.onPageChanged.addRules([newRule]);
+                // });
                 var allowedDomains = text.split('\n');
                 for (i = 0; i < allowedDomains.length; i++) {
                     var item = allowedDomains[i].trim();

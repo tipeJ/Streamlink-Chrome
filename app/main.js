c5192d4a54ab441f47aaa99785ba5e2f2f29bd77
tipej <tiitus.jarvinen@gmail.com>
2020-08-24 01:21:57 +0300

    Added favourites

--- app/main.js
+++ app/main.js
@@ -8,6 +8,12 @@ function launchStream() {
   });
 }
 
+function launchSpecificStream(url) {
+  chrome.runtime.sendMessage({Message: url}, function (response) {
+    window.close();
+  });
+}
+
 function openSettings() {
   chrome.runtime.sendMessage({Message: "prefs"}, function (response) {
     window.close();
@@ -15,9 +21,53 @@ function openSettings() {
   
 }
 
+function addFavourite() {
+  console.log("BEGINFAVOURITE");
+  chrome.storage.local.get(null, function(items) {
+    var allKeys = Object.keys(items);
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        var currentUrl = tabs[0].url;
+        if (allKeys[currentUrl] == null) {
+          chrome.storage.local.set({[currentUrl]: [tabs[0].title]}, function() {
+            console.log("Added favourite");
+          });
+        }
+    });
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('send-message-button').addEventListener(
       'click', launchStream);
-    document.getElementById('settings-button').addEventListener(
-      'click', openSettings);
+  document.getElementById('settings-button').addEventListener(
+    'click', openSettings);
+  document.getElementById('add-favourite-button').addEventListener(
+    'click', addFavourite);
+  chrome.storage.local.get(null, function(items) {
+    var allKeys = Object.keys(items);
+    var allValues = Object.values(items);
+    for (var i = 0; i < allKeys.length; i++) {
+      var para = document.createElement("p");
+
+      var link = document.createElement("a");
+      link.innerHTML = allValues[i];
+      link.setAttribute('href', allKeys[i]);
+      link.setAttribute('target', "_blank");
+      para.appendChild(link);
+
+      var removeButton = document.createElement("button");
+      removeButton.innerHTML = "X";
+      para.append(removeButton);
+
+      var playButton = document.createElement("button");
+      playButton.addEventListener('click', function(){
+        launchSpecificStream(allKeys[i]);
+      });
+      playButton.innerHTML = ">";
+      para.append(playButton);
+      
+      document.getElementById('favourites-container').appendChild(para);
+    }
+  });
 });
+
--- app/twitch_inject.js
+++ app/twitch_inject.js
@@ -1,12 +1,17 @@
-var launchButton = document.createElement("BUTTON");
-launchButton.addEventListener("click", function() {
-    chrome.runtime.sendMessage({Message: "launchStream"}, function (response) {
+try {
+    var launchButton = document.createElement("BUTTON");
+    launchButton.addEventListener("click", function() {
+        chrome.runtime.sendMessage({Message: "launchStream"}, function (response) {
+        });
     });
-});
-var imgURL = chrome.extension.getURL("assets/icon-22.png");
-launchButton.innerHTML = "<img src=" + imgURL + ">";
-launchButton.setAttribute("aria-label", "Streamlink");
-launchButton.setAttribute("title", "Streamlink");
-launchButton.className = "tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-button-icon tw-button-icon--overlay tw-core-button tw-core-button--overlay tw-inline-flex tw-interactive tw-justify-content-center tw-overflow-hidden tw-relative";
-document.getElementsByClassName('player-controls__right-control-group tw-align-items-center tw-flex tw-flex-grow-1 tw-justify-content-end')[0]
-    .appendChild(launchButton);
+    var imgURL = chrome.extension.getURL("assets/icon-22.png");
+    launchButton.innerHTML = "<img src=" + imgURL + ">";
+    launchButton.setAttribute("aria-label", "Streamlink");
+    launchButton.setAttribute("title", "Streamlink");
+    launchButton.className = "tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-button-icon tw-button-icon--overlay tw-core-button tw-core-button--overlay tw-inline-flex tw-interactive tw-justify-content-center tw-overflow-hidden tw-relative";
+    document.getElementsByClassName('player-controls__right-control-group tw-align-items-center tw-flex tw-flex-grow-1 tw-justify-content-end')[0]
+        .appendChild(launchButton);
+}
+catch (err) {
+    console.log(err);
+}
